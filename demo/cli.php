cd808da127832f36316b7b861f8144f6d5886122
A1essandro <alexander.yermakov.1990@gmail.com>
2016-01-13 15:02:15 +0400

    Added report of memory usage to CLI demo

--- demo/cli.php
+++ demo/cli.php
@@ -4,9 +4,12 @@ ini_set('display_errors', 'On');
 require_once __DIR__ . '/../src/PerlinNoiseGenerator.php';
 $gen = new MapGenerator\PerlinNoiseGenerator();
 
+$memStart = memory_get_usage();
 $gen->setPersistence(0.5);
-$gen->setSize(750);
+$gen->setSize(1025);
 $gen->setMapSeed('seed');
 $gen->generate();
 
-echo sprintf('Time: %s', round(microtime(true) - $start, 3));
+echo sprintf('Memory Peak Usage: %sMB', round(memory_get_peak_usage() / 1024 / 1024, 2)) . PHP_EOL;
+echo sprintf('Memory Usage: %sMB', round((memory_get_usage() - $memStart) / 1024 / 1024, 2)) . PHP_EOL;
+echo sprintf('Time: %s', round(microtime(true) - $start, 3)) . PHP_EOL;
